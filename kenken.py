22b3e83ccc8de7b8f8487d1a9ab3c857a6b3fcc3
Mark Essam Hakim <63367712+markessam12@users.noreply.github.com>
2022-04-30 21:44:31 +0200

    Tasks Assigned for each individual

--- kenken.py
+++ kenken.py
@@ -3,11 +3,13 @@ import random
 class Kenken:
     def __init__(self):
         """
-        grid: 2d list of current existing numbers
+        grid: 2d list of current existing numbers, zeros for empty cells
         cage: dictionary of dictionaries representing cages in the kenken puzzle. ex: {1:{value:5,op:'+',cells:[(0,0),(0,1),(1,0)]}}
         """
+        #Zamala needs to type a hardcoded example here for trials
         (self.grid, self.cage) = self.generate(self,random.randint(3, 9))
 
+    #Tarek and Zamala
     def generate(self, size):
         """
         :param size: the size of the generated grid
@@ -15,23 +17,27 @@ class Kenken:
         """
         pass
 
-    def bounding(self):
+    #Engy and Eman, Engy will start on the gui after her work on the constrain checking functions
+    def Bounding(self, row, col, value):
         """
-        responsible for Column checking, Row checking, and cage checking
+        responsible for Column checking, Row checking, and Grid checking
         :return: returns true if the current chosen numbers satisfy the game rules and false otherwise
         """
         pass
 
+    #Engy
     def check_row(self, row, value):
         pass
 
+    #Engy
     def check_column(self, col, value):
         pass
 
+    #Eman
     def check_cage(self, raw, col, value):
         pass
 
-
+    #Mark and Mark
     def solve(self, forward_check=True, arc_consistency=True):
         """
         a wrapper function that calls the recursive backtracking function
@@ -41,11 +47,14 @@ class Kenken:
         """
         pass
 
+    #Mark and Mark
     def backtracking(self, forward_check, arc_consistency):
         pass
 
+    #Zamala
     def print(self):
         pass
 
+    #Engy
     def find_empty(self):
         pass
