11ea067b9f9a5cde7a96a18b40ec90e32c12b9ea
markmourad26 <63330455+markmourad26@users.noreply.github.com>
2022-05-16 00:17:02 +0200

    fixing forward checking and cage checking

--- kenken.py
+++ kenken.py
@@ -1,4 +1,5 @@
 import random
+from hardCodedExamples import *
 
 class Kenken:
     def __init__(self, n=3):
@@ -15,15 +16,20 @@ class Kenken:
             5: {"value" :4, "op":'+',   "cells":[(1,2),(2,2)]}
          }
         #Solution is [[1,3,2],[3,2,1],[2,1,3]]
-        self.grid = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+        self.cages = cages # Take hardcode example, remove when generate function is done.
         # enable generate again when it's ready and disable the hardcoded example
+
+        self.grid = [[0 for i in range(n)] for j in range(n)]
+
         # (self.grid, self.cages) = self.generate(self,random.randint(3, 9))
-        self.n = len(self.grid)
-        #3D list for the domain of available values for each cell (1d for each cell in the 2d grid)
-        self.domains = [[[True for i in range(1, self.n+1)]]*self.n]*self.n
+        self.n = n
+
         # --- 2D array, each cell position has cage number as its value ---
         self.cellToCageMap = self.mapCellsToCages()
 
+        #3D list for the domain of available values for each cell (1d for each cell in the 2d grid)
+        self.domains = [[[True for i in range(n)] for j in range(n)] for k in range(n)]
         """
         Example:
         [
@@ -33,6 +39,7 @@ class Kenken:
         ]
         """
 
+
     #Tarek and Zamala
     def generate(self, size):
         """
@@ -41,7 +48,7 @@ class Kenken:
         """
         pass
 
-    #Engy and Eman, Engy will start on the gui after her work on the constrain checking functions
+    
     def Bounding(self, row, col, value):
         """
         Responsible for Column checking, Row checking, and Grid checking
@@ -62,7 +69,7 @@ class Kenken:
 
         return isAllConstraintsApplied
 
-    #Engy
+    
     def check_row(self, row, value):
         """
         This function returns true iff row has no cell containing value same as the given value,
@@ -83,7 +90,7 @@ class Kenken:
         return isConstraintApplied
 
 
-    #Engy
+    
     def check_column(self, col, value):
         """
         This function returns true iff col has no cell containing value same as the given value,
@@ -103,7 +110,7 @@ class Kenken:
 
         return isConstraintApplied
 
-    #Eman
+    
     def check_cage(self, row, col, value):
          
         """This function takes parameters specifies cell position and the value inside this cell,
@@ -129,45 +136,64 @@ class Kenken:
         # ADDITION
         elif cageOperation == "+" : # cellsList > = 2 
             summationResult = 0
+            zerosCount = 0
             for cell in cellsList: # cell is tuple (1,2) 
-                if (row,col) != cell: # sum all cells except the one Iam checking 
+                if (row,col) != cell: # sum all cells except the one Iam checking
+                    zerosCount += (self.grid[cell[0]][cell[1]] == 0)
                     summationResult += int(self.grid[cell[0]][cell[1]])
-            if summationResult + int(value) <= int(cageNeededToBeChecked['value']):
+            if zerosCount and summationResult + int(value) < int(cageNeededToBeChecked['value']):
+                isConstraintApplied = True
+            elif not zerosCount and summationResult + int(value) == int(cageNeededToBeChecked['value']):
                 isConstraintApplied = True
 
         # SUBTRACTION 
         elif cageOperation == "-": # cellsList has only 2 cells according to game Rules
             subtractionResult = 0
             for cell in cellsList:
-                if (row,col) != cell : # I have this cell value passed to my function 
+                if (row,col) != cell : # I have this cell value passed to my function
+                    if self.grid[cell[0]][cell[1]] == 0:
+                        if int(value) == cageNeededToBeChecked['value']:
+                            isConstraintApplied = False
+                        else:
+                            isConstraintApplied = True
+                        break
                     subtractionResult = int(value) - int(self.grid[cell[0]][cell[1]])
             # check if constraint is applied
-            if abs(subtractionResult) <= cageNeededToBeChecked['value']:
+            if abs(subtractionResult) == cageNeededToBeChecked['value']:
                 isConstraintApplied = True
 
         # MULTIPLICATION 
         elif cageOperation == "*": # cellsList > = 2 
             multiplicationResult = 1
+            zerosCount = 0
             for cell in cellsList:
                 if (row,col) != cell:
-                    multiplicationResult *= self.grid[cell[0]][cell[1]]
+                    if self.grid[cell[0]][cell[1]] == 0:
+                        zerosCount += 1
+                    else:
+                        multiplicationResult *= self.grid[cell[0]][cell[1]]
 
-            if multiplicationResult * value <= cageNeededToBeChecked['value']:
+            if zerosCount and multiplicationResult * value <= cageNeededToBeChecked['value']:
+                isConstraintApplied = True
+            elif not zerosCount and multiplicationResult * value == cageNeededToBeChecked['value']:
                 isConstraintApplied = True
 
         # DIVISION 
         elif cageOperation == "/": # cellsList = 2
             divisionResult = 1
             for cell in cellsList:
-                if (row,col) != cell : # I have this cell value passed to my function 
-                    if value > self.grid[cell[0]][cell[1]]:
+                if (row,col) != cell : # I have this cell value passed to my function
+                    if self.grid[cell[0]][cell[1]] == 0:
+                        isConstraintApplied = True
+                        break
+                    elif value > self.grid[cell[0]][cell[1]]:
                         # divide greater/smaller
                         divisionResult = value / self.grid[cell[0]][cell[1]]
                     else:
                         # divide greater/smaller
                         divisionResult = self.grid[cell[0]][cell[1]] / value
             # check if constraint is applied
-            if divisionResult <= cageNeededToBeChecked['value']:
+            if divisionResult == cageNeededToBeChecked['value']:
                 isConstraintApplied = True
 
         return isConstraintApplied
@@ -185,14 +211,15 @@ class Kenken:
 
         return cellToCageMap
 
-    #Mark and Mark
-    def solve(self, forward_check=True, arc_consistency=False):
+    
+    def solve(self, forward_check = False, arc_consistency=False):
         """
         a wrapper function that calls the recursive backtracking function
         :param forward_check: enable forward checking mode
         :param arc_consistency: enable arc consistency mode
         :return:
         """
+
         if forward_check and not arc_consistency:
             self.backtracking_FC()
         elif forward_check and arc_consistency:
@@ -200,7 +227,7 @@ class Kenken:
         elif not forward_check and not arc_consistency:
             self.backtracking()
 
-    #Mark and Mark
+    
     def backtracking(self):
         row, col = self.find_empty()
         # Base case
@@ -226,15 +253,15 @@ class Kenken:
             return True
 
         # iterate over all possible values in the domain to test them
-        for value in range(1, self.n+1):
+        for value in range(self.n):
             # check if the current value is in the domain of the cell
             if self.domains[row][col][value]:
                 # check if the current value will obey all constrains
-                if self.Bounding(row, col, value):
-                    self.grid[row][col] = value
+                if self.Bounding(row, col, value + 1):
+                    self.grid[row][col] = value + 1
                     # forward checking the current value in all the neighbouring cells
                     self.forward_check(row, col, value, setting=False)
-                    if self.backtracking():
+                    if self.backtracking_FC():
                         return True
                     # return the removed value to the domains of the neighbouring cells
                     self.forward_check(row, col, value, setting=True)
@@ -244,16 +271,17 @@ class Kenken:
         return False
 
     def forward_check(self, row, col, value, setting=False):
-        for i in range(0, self.n):
+        for i in range(self.n):
             self.domains[i][col][value] = setting
             self.domains[row][i][value] = setting
+
 
 
     #Zamala
     def print(self):
         return self.grid
 
-    # Engy
+    
     def find_empty(self):
         """
         :return: first empty position to use it next
@@ -269,7 +297,7 @@ class Kenken:
                     # --- return first empty position to work on it next ---
                     return (row,col)
         return (None,None)
-    #Engy
+    
     def findListOfEmptyPositions(self):
         """
         This list return list of tuples containing all cells' positions has "0"(zero) value.
--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from kenken import Kenken
 import time
 
-t1 = time.time_ns()
-for i in range(100):
-    game1 = Kenken(3)
-    game1.solve()
+t1 = time.time()
+game1 = Kenken(9)
+game1.solve(forward_check = False)
 
-print((time.time_ns()-t1))
+print((time.time()-t1))
 print(game1.print())
